4a0cc27c4caea98876d0ec06d8e2471f1deddc3b
TwDnCTX <sczljy@foxmail.com>
2025-12-11 18:54:15 +0800

    Update deploy.sh

--- termux-scripts/deploy.sh
+++ termux-scripts/deploy.sh
@@ -87,7 +87,7 @@ install_python_packages() {
     print_info "安装 Python 依赖包..."
     
     # 升级 pip
-    pip install --upgrade pip
+    # pip install --upgrade pip
     
     # 安装依赖
     pip install pillow openai requests
